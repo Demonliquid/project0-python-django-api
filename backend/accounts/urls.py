8735e3b6ac44216b6eedadb6ca6a2062ee2d09e8
Fernandez <Martinandresfernandez96@gmail.com>
2021-09-23 21:29:11 -0300

    This commit dockerizes accounts

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, re_path
 from dj_rest_auth.registration.views import RegisterView, VerifyEmailView, ConfirmEmailView
-from dj_rest_auth.views import LoginView, LogoutView, UserDetailsView
+from dj_rest_auth.views import LoginView, LogoutView, UserDetailsView, PasswordResetView, PasswordResetConfirmView
 
 
 urlpatterns = [
@@ -16,4 +16,8 @@ urlpatterns = [
          VerifyEmailView.as_view(), name='account_email_verification_sent'),
     re_path(r'^account-confirm-email/(?P<key>[-:\w]+)/$',
             VerifyEmailView.as_view(), name='account_confirm_email'),
+
+    path('password-reset/', PasswordResetView.as_view()),
+    path('password-reset-confirm/<slug:uidb64>/<slug:token>/',
+         PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
 ]
--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,12 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from dj_rest_auth.views import PasswordResetView, PasswordResetConfirmView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('accounts/', include('accounts.urls')),
-    path('password-reset/', PasswordResetView.as_view()),
-    path('password-reset-confirm/<slug:uidb64>/<slug:token>/',
-         PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
 ]
